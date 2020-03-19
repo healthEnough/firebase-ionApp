f02ea360bfe60ebcfc82b09721a20fc0266ead35
Guilherme Eduardo <health.bin.enough@gmail.com>
2020-03-18 21:02:50 -0300

    Commit inicaial
    Feitos:  Login e Cadastro
    Andamento: Tabs e Routing

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyCByirEgHNRAo0wCyyDbPJCPlFxrWktLt4",
+    authDomain: "aaaa-8b15b.firebaseapp.com",
+    databaseURL: "https://aaaa-8b15b.firebaseio.com",
+    projectId: "aaaa-8b15b",
+    storageBucket: "aaaa-8b15b.appspot.com",
+    messagingSenderId: "267371029227",
+    appId: "1:267371029227:web:ab4e88de1d7f12485c54ae",
+    measurementId: "G-GZ1G51YEFJ"
+};
+
+export default config
---
+++ src/app/register/register.page.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  username: string = ""
+  password: string = ""
+  cpassword: string = ""
+
+  constructor(
+    public afAuth: AngularFireAuth,
+    public alert: AlertController,
+    public router: Router
+    ) { }
+
+  ngOnInit() {}
+
+  async register(){
+    const { username, password, cpassword } = this
+    if(password !== cpassword){
+      this.showAlert("Error!", "Password don't match")
+      return console.error("Password don't match")
+    } try{
+        const res = await this.afAuth.auth.createUserWithEmailAndPassword(username + '@gmail.com', password)
+        console.log(res)
+        this.showAlert("Success!", "Welcome aboard!")
+        this.router.navigate(['/tabs'])
+    } catch(error){
+        console.dir(error)
+        this.showAlert("Error!", error.message)
+    }
+  }
+
+  async showAlert(header: string, message: string){
+    const alert = this.alert.create({
+      header,
+      message,
+      buttons: ["Ok"]
+    })
+
+    await (await alert).present()
+
+  }
+
+}
